6a72acc5a88fa8f22c5036e13d0ea528490cf493
Game <yusr96@outlook.com>
2021-02-26 09:46:25 +0900

    2/26

---
+++ 吉田学園情報ビジネス専門学校_余時澤/個人制作3 2021-2/Code/Menu.cpp
@@ -0,0 +1,168 @@
+//=============================================================================
+//
+// [Menu.cpp]
+// Author : JITAKU YO
+//
+//=============================================================================
+#include "Menu.h"
+#include "Fade.h"
+#include "Dinput8.h"
+#include "Enemy.h"
+#include "Player.h"
+
+LPDIRECT3DVERTEXBUFFER9 g_pVtxBuffMenu = NULL;
+LPDIRECT3DTEXTURE9 g_pTextureMenu[MENU_COMPONENT] = {};
+POLYGON_2D *pMenu;
+int g_nCntTime3;
+HRESULT InitMenu(void)
+{
+	g_nCntTime3 = 0;
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();
+	VERTEX_2D *pVtx;
+	pMenu = (POLYGON_2D *)malloc(MENU_COMPONENT * sizeof(POLYGON_2D));
+
+	if (FAILED(D3DXCreateTextureFromFile(pDevice, "data/Texture/ScreenTransition/SelectHand.png", &g_pTextureMenu[0])))return E_FAIL;
+	pMenu[0].bUse = true;
+	pMenu[0].pos = D3DXVECTOR3(WIDTH_SCREEN / 2 - 250 , HEIGHT_SCREEN / 2, 0);
+	pMenu[0].size = D3DXVECTOR2(WIDTH_SCREEN / 30, HEIGHT_SCREEN / 25);
+
+	if (FAILED(D3DXCreateTextureFromFile(pDevice, "data/Texture/ScreenTransition/Start.png", &g_pTextureMenu[1])))return E_FAIL;
+	pMenu[1].bUse = true;
+	pMenu[1].pos = D3DXVECTOR3(WIDTH_SCREEN / 2 , HEIGHT_SCREEN / 2, 0);
+	pMenu[1].size = D3DXVECTOR2(WIDTH_SCREEN / 12, HEIGHT_SCREEN / 8);
+
+	if (FAILED(D3DXCreateTextureFromFile(pDevice, "data/Texture/ScreenTransition/Tutorial.png", &g_pTextureMenu[2])))return E_FAIL;
+	pMenu[2].bUse = true;
+	pMenu[2].pos = D3DXVECTOR3(WIDTH_SCREEN / 2 , HEIGHT_SCREEN / 2 + 150, 0);
+	pMenu[2].size = D3DXVECTOR2(WIDTH_SCREEN / 8, HEIGHT_SCREEN / 6);
+
+	if (FAILED(D3DXCreateTextureFromFile(pDevice, "data/Texture/ScreenTransition/Ranking.png", &g_pTextureMenu[3])))return E_FAIL;
+	pMenu[3].bUse = false;
+	pMenu[3].pos = D3DXVECTOR3(WIDTH_SCREEN / 2 , HEIGHT_SCREEN / 2 + 150, 0);
+	pMenu[3].size = D3DXVECTOR2(WIDTH_SCREEN / 12, HEIGHT_SCREEN / 8);
+
+	if (FAILED(D3DXCreateTextureFromFile(pDevice, "data/Texture/ScreenTransition/Exit.png", &g_pTextureMenu[4])))return E_FAIL;
+	pMenu[4].bUse = true;
+	pMenu[4].pos = D3DXVECTOR3(WIDTH_SCREEN / 2 , HEIGHT_SCREEN / 2 + 300, 0);
+	pMenu[4].size = D3DXVECTOR2(WIDTH_SCREEN / 12, HEIGHT_SCREEN / 8);
+
+	//create vertex buffer
+	if (FAILED(pDevice->CreateVertexBuffer(sizeof(VERTEX_2D) * 4 * MENU_COMPONENT, D3DUSAGE_WRITEONLY, FVF_VERTEX_2D, D3DPOOL_MANAGED, &g_pVtxBuffMenu, NULL))) return E_FAIL;
+
+	g_pVtxBuffMenu->Lock(0, 0, (void**)&pVtx, 0);
+	for (int nCnt = 0; nCnt < MENU_COMPONENT; nCnt++, pVtx += 4)
+	{
+		pVtx[0].pos = D3DXVECTOR3(pMenu[nCnt].pos.x - pMenu[nCnt].size.x, pMenu[nCnt].pos.y - pMenu[nCnt].size.y, 0.0f);
+		pVtx[1].pos = D3DXVECTOR3(pMenu[nCnt].pos.x + pMenu[nCnt].size.x, pMenu[nCnt].pos.y - pMenu[nCnt].size.y, 0.0f);
+		pVtx[2].pos = D3DXVECTOR3(pMenu[nCnt].pos.x - pMenu[nCnt].size.x, pMenu[nCnt].pos.y + pMenu[nCnt].size.y, 0.0f);
+		pVtx[3].pos = D3DXVECTOR3(pMenu[nCnt].pos.x + pMenu[nCnt].size.x, pMenu[nCnt].pos.y + pMenu[nCnt].size.y, 0.0f);
+		pVtx[0].rhw = 1.0f;
+		pVtx[1].rhw = 1.0f;
+		pVtx[2].rhw = 1.0f;
+		pVtx[3].rhw = 1.0f;
+		pVtx[0].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		pVtx[1].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		pVtx[2].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		pVtx[3].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		pVtx[0].tex = D3DXVECTOR2(0.0, 0.0);
+		pVtx[1].tex = D3DXVECTOR2(1.0, 0.0);
+		pVtx[2].tex = D3DXVECTOR2(0.0, 1.0);
+		pVtx[3].tex = D3DXVECTOR2(1.0, 1.0);
+	}
+	g_pVtxBuffMenu->Unlock();
+
+	return S_OK;
+}
+
+void UninitMenu(void)
+{
+	if (g_pVtxBuffMenu != NULL)
+	{			 
+		g_pVtxBuffMenu->Release();
+		g_pVtxBuffMenu = NULL;
+	}
+	for (int nCnt = 0; nCnt < MENU_COMPONENT; nCnt++)
+	{
+		if (g_pTextureMenu[nCnt] != NULL)
+		{			  
+			g_pTextureMenu[nCnt]->Release();
+			g_pTextureMenu[nCnt] = NULL;
+		}
+	}
+	free(pMenu);
+}
+
+void UpdateMenu(void)
+{
+	VERTEX_2D *pVtx;
+	g_nCntTime3++;
+
+	if (GetkeyboardTrigger(DIK_W) == true)
+	{
+		pMenu[0].pos.y -= 150;
+		if (pMenu[0].pos.y < HEIGHT_SCREEN / 2) pMenu[0].pos.y = HEIGHT_SCREEN / 2 + 300;
+	}
+	if (GetkeyboardTrigger(DIK_S) == true)
+	{
+		pMenu[0].pos.y += 150;
+		if (pMenu[0].pos.y > HEIGHT_SCREEN / 2 + 300) pMenu[0].pos.y = HEIGHT_SCREEN / 2;
+	}
+
+	if (GetkeyboardTrigger(DIK_RETURN) == true)
+	{
+		int nPosy = (int)pMenu[0].pos.y;
+		switch (nPosy)
+		{
+		case  HEIGHT_SCREEN / 2:
+			SetFade(FADE_OUT, MODE_GAME);
+			break;
+		case  HEIGHT_SCREEN / 2 + 150:
+			SetFade(FADE_OUT, MODE_TUTORIAL);
+			break;
+		//case  HEIGHT_SCREEN / 2 + 150:
+		//	SetFade(FADE_OUT, MODE_RANKING);
+		//	break;
+		case  HEIGHT_SCREEN / 2 + 300:
+			PostQuitMessage(0);
+			break;
+		}
+	}
+
+	g_pVtxBuffMenu->Lock(0, 0, (void**)&pVtx, 0);
+	for (int nCnt = 0; nCnt < MENU_COMPONENT; nCnt++, pVtx += 4)
+	{
+		pVtx[0].pos = D3DXVECTOR3(pMenu[nCnt].pos.x - pMenu[nCnt].size.x, pMenu[nCnt].pos.y - pMenu[nCnt].size.y, 0.0f);
+		pVtx[1].pos = D3DXVECTOR3(pMenu[nCnt].pos.x + pMenu[nCnt].size.x, pMenu[nCnt].pos.y - pMenu[nCnt].size.y, 0.0f);
+		pVtx[2].pos = D3DXVECTOR3(pMenu[nCnt].pos.x - pMenu[nCnt].size.x, pMenu[nCnt].pos.y + pMenu[nCnt].size.y, 0.0f);
+		pVtx[3].pos = D3DXVECTOR3(pMenu[nCnt].pos.x + pMenu[nCnt].size.x, pMenu[nCnt].pos.y + pMenu[nCnt].size.y, 0.0f);
+		pVtx[0].rhw = 1.0f;
+		pVtx[1].rhw = 1.0f;
+		pVtx[2].rhw = 1.0f;
+		pVtx[3].rhw = 1.0f;
+		pVtx[0].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		pVtx[1].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		pVtx[2].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		pVtx[3].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		pVtx[0].tex = D3DXVECTOR2(0.0, 0.0);
+		pVtx[1].tex = D3DXVECTOR2(1.0, 0.0);
+		pVtx[2].tex = D3DXVECTOR2(0.0, 1.0);
+		pVtx[3].tex = D3DXVECTOR2(1.0, 1.0);
+	}
+	g_pVtxBuffMenu->Unlock();
+}
+
+void DrawMenu(void)
+{
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();
+	pDevice->SetStreamSource(0, g_pVtxBuffMenu, 0, sizeof(VERTEX_2D));
+	pDevice->SetFVF(FVF_VERTEX_2D);
+
+	for (int nCnt = 0; nCnt < MENU_COMPONENT; nCnt++)
+	{
+		if (pMenu[nCnt].bUse == true)
+		{
+			pDevice->SetTexture(0, g_pTextureMenu[nCnt]);
+			pDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, nCnt * 4, 2);
+		}
+	}
+}
---
+++ 吉田学園情報ビジネス専門学校_余時澤/個人制作3 2021-2/Code/WorldModel.cpp
@@ -0,0 +1,284 @@
+//=============================================================================
+//
+// [WorldModel.cpp]
+// Author : JITAKU YO
+//
+//=============================================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include "WorldModel.h"
+#include "Camera.h"
+#include "Collision.h"
+
+MODEL *pWorldModel;
+D3DXMATERIAL *pMaterials;
+COLLISION_BOX *pCol_Box;
+void InitWorldModel(void)
+{
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();
+	//VERTEX_3D *pVtx;
+	pWorldModel = (MODEL*)malloc(WORLDMODEL_NUM * sizeof(MODEL));
+	pCol_Box = (COLLISION_BOX*)malloc(WORLDMODEL_NUM * sizeof(COLLISION_BOX));
+	SetWorldModel();
+
+	//材質とUVテクスチャを読み込む
+	for (int nCnt1 = 0; nCnt1 < WORLDMODEL_NUM; nCnt1++)
+	{
+		pMaterials = (D3DXMATERIAL*)pWorldModel[nCnt1].Xfile_Materials->GetBufferPointer();
+		for (DWORD nCnt = 0; nCnt < (int)pWorldModel[nCnt1].Xfile_MaterialNum; nCnt++)
+		{
+			if (pMaterials[nCnt].pTextureFilename != NULL)
+			{
+				char* file_name = pMaterials[nCnt].pTextureFilename;
+				LPDIRECT3DTEXTURE9 texture = NULL;
+				D3DXCreateTextureFromFileA(pDevice,
+					file_name,
+					&pWorldModel[nCnt1].g_Textures[nCnt]);
+			}
+			else
+			{
+				pWorldModel[nCnt1].g_Textures[nCnt] = nullptr;
+			}
+		}
+	}
+
+	//当たり判定用boxの設定
+	int nNumVtx;//頂点数
+	DWORD sizeFvF;//一つ頂点formatのsize
+	BYTE *pVtxBuff;//頂点bufferへのpointer
+	for (int nCnt = 0; nCnt < WORLDMODEL_NUM; nCnt++)
+	{
+		nNumVtx = pWorldModel[nCnt].Xfile_Mesh->GetNumVertices();//頂点数を取る
+		sizeFvF = D3DXGetFVFVertexSize(pWorldModel[nCnt].Xfile_Mesh->GetFVF());//formatの大きさ
+		pWorldModel[nCnt].Xfile_Mesh->LockVertexBuffer(D3DLOCK_READONLY, (void**)&pVtxBuff);//頂点buffをlock
+		for (int nCntVtx = 0; nCntVtx < nNumVtx; nCntVtx++)
+		{
+			D3DXVECTOR3 vtx = *(D3DXVECTOR3*)pVtxBuff;//頂点情報を取る
+			pVtxBuff += sizeFvF;//次に行く
+
+			//当たり判定用boxの頂点を探す
+			if (nCntVtx == 0)//初めの頂点
+			{
+				pWorldModel[nCnt].MaxColBox = vtx;
+				pWorldModel[nCnt].MinColBox = vtx;
+			}
+			else
+			{
+				if (vtx.x > pWorldModel[nCnt].MaxColBox.x) pWorldModel[nCnt].MaxColBox.x = vtx.x;
+				else if (vtx.x < pWorldModel[nCnt].MinColBox.x) pWorldModel[nCnt].MinColBox.x = vtx.x;
+				if (vtx.y > pWorldModel[nCnt].MaxColBox.y) pWorldModel[nCnt].MaxColBox.y = vtx.y;
+				else if (vtx.y < pWorldModel[nCnt].MinColBox.y) pWorldModel[nCnt].MinColBox.y = vtx.y;
+				if (vtx.z > pWorldModel[nCnt].MaxColBox.z) pWorldModel[nCnt].MaxColBox.z = vtx.z;
+				else if (vtx.z < pWorldModel[nCnt].MinColBox.z) pWorldModel[nCnt].MinColBox.z = vtx.z;
+			}
+		}
+		pWorldModel[nCnt].MaxColBox = pWorldModel[nCnt].pos + pWorldModel[nCnt].MaxColBox;
+		pWorldModel[nCnt].MinColBox = pWorldModel[nCnt].pos + pWorldModel[nCnt].MinColBox;//minboxの値は負数
+		pWorldModel[nCnt].Xfile_Mesh->UnlockVertexBuffer();//unlock
+	}
+}
+
+void SetWorldModel(void)
+{
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();
+	for (int nCnt = 0; nCnt < WORLDMODEL_NUM; nCnt++)
+	{
+		if (nCnt % 2 == 0)
+		{
+			pWorldModel[nCnt].rot = D3DXVECTOR3(0.f, 0.f, 0.f);
+			pWorldModel[nCnt].pos.x = (float)(rand() % WORLDMODEL_POS_RANGE_X + WORLDMODEL_POS_RANGE_X);
+		}
+		else
+		{
+			pWorldModel[nCnt].rot = D3DXVECTOR3(0.f, D3DX_PI, 0.f);
+			pWorldModel[nCnt].pos.x = (float)(rand() % WORLDMODEL_POS_RANGE_X - 2 * WORLDMODEL_POS_RANGE_X);
+		}
+	
+		pWorldModel[nCnt].pos.z = (float)(rand() % WORLDMODEL_POS_RANGE_Z - WORLDMODEL_POS_RANGE_Z / 2);
+		pWorldModel[nCnt].pos.y = 0;
+		pWorldModel[nCnt].move = D3DXVECTOR3(0.f, 0.f, 0.f);
+		pWorldModel[nCnt].nID = rand () % 3;
+		switch (pWorldModel[nCnt].nID)
+		{
+		case 0:
+			D3DXLoadMeshFromX("data/MODEL/Car/Car0.x", D3DXMESH_SYSTEMMEM, pDevice, NULL, &pWorldModel[nCnt].Xfile_Materials, NULL, &pWorldModel[nCnt].Xfile_MaterialNum, &pWorldModel[nCnt].Xfile_Mesh);
+			break;
+		case 1:
+			D3DXLoadMeshFromX("data/MODEL/Car/Car2.x", D3DXMESH_SYSTEMMEM, pDevice, NULL, &pWorldModel[nCnt].Xfile_Materials, NULL, &pWorldModel[nCnt].Xfile_MaterialNum, &pWorldModel[nCnt].Xfile_Mesh);
+			break;
+		case 2:
+			D3DXLoadMeshFromX("data/MODEL/Car/Car3.x", D3DXMESH_SYSTEMMEM, pDevice, NULL, &pWorldModel[nCnt].Xfile_Materials, NULL, &pWorldModel[nCnt].Xfile_MaterialNum, &pWorldModel[nCnt].Xfile_Mesh);
+			break;
+		case 3:
+			D3DXLoadMeshFromX("data/MODEL/Car/Car1.x", D3DXMESH_SYSTEMMEM, pDevice, NULL, &pWorldModel[nCnt].Xfile_Materials, NULL, &pWorldModel[nCnt].Xfile_MaterialNum, &pWorldModel[nCnt].Xfile_Mesh);
+			break;
+		default:
+			break;
+		}
+		pWorldModel[nCnt].bMove = true;
+		pWorldModel[nCnt].bUse = true;
+		pWorldModel[nCnt].fMoveSpeed = 0.f;
+	}
+}
+
+void UninitWorldModel(void)
+{
+	free(pWorldModel);
+}
+
+void UpdateWorldModel(void)
+{
+	for (int nCnt = 0; nCnt < WORLDMODEL_NUM; nCnt++)
+	{
+		if (pWorldModel[nCnt].bMove == true)
+		{
+			if (nCnt % 2 == 0)
+			{
+				if (pWorldModel[nCnt].pos.x <= -WORLDMODEL_POS_RANGE_X)
+				{
+					pWorldModel[nCnt].pos.x = WORLDMODEL_POS_RANGE_X;
+					pWorldModel[nCnt].pos.z = (float)(rand() % WORLDMODEL_POS_RANGE_Z - WORLDMODEL_POS_RANGE_Z / 2);
+
+					//当たり判定用boxの設定
+					int nNumVtx;//頂点数
+					DWORD sizeFvF;//一つ頂点formatのsize
+					BYTE *pVtxBuff;//頂点bufferへのpointer
+					nNumVtx = pWorldModel[nCnt].Xfile_Mesh->GetNumVertices();//頂点数を取る
+					sizeFvF = D3DXGetFVFVertexSize(pWorldModel[nCnt].Xfile_Mesh->GetFVF());//formatの大きさ
+					pWorldModel[nCnt].Xfile_Mesh->LockVertexBuffer(D3DLOCK_READONLY, (void**)&pVtxBuff);//頂点buffをlock
+					for (int nCntVtx = 0; nCntVtx < nNumVtx; nCntVtx++)
+					{
+						D3DXVECTOR3 vtx = *(D3DXVECTOR3*)pVtxBuff;//頂点情報を取る
+						pVtxBuff += sizeFvF;//次に行く
+
+						//当たり判定用boxの頂点を探す
+						if (nCntVtx == 0)//初めの頂点
+						{
+							pWorldModel[nCnt].MaxColBox = vtx;
+							pWorldModel[nCnt].MinColBox = vtx;
+						}
+						else
+						{
+							if (vtx.x > pWorldModel[nCnt].MaxColBox.x) pWorldModel[nCnt].MaxColBox.x = vtx.x;
+							else if (vtx.x < pWorldModel[nCnt].MinColBox.x) pWorldModel[nCnt].MinColBox.x = vtx.x;
+							if (vtx.y > pWorldModel[nCnt].MaxColBox.y) pWorldModel[nCnt].MaxColBox.y = vtx.y;
+							else if (vtx.x < pWorldModel[nCnt].MinColBox.x) pWorldModel[nCnt].MinColBox.y = vtx.y;
+							if (vtx.z > pWorldModel[nCnt].MaxColBox.z) pWorldModel[nCnt].MaxColBox.z = vtx.z;
+							else if (vtx.x < pWorldModel[nCnt].MinColBox.x) pWorldModel[nCnt].MinColBox.z = vtx.z;
+						}
+					}
+					pWorldModel[nCnt].MaxColBox = pWorldModel[nCnt].pos + pWorldModel[nCnt].MaxColBox;
+					pWorldModel[nCnt].MinColBox = pWorldModel[nCnt].pos + pWorldModel[nCnt].MinColBox;//minboxの値は負数
+					pWorldModel[nCnt].Xfile_Mesh->UnlockVertexBuffer();//unlock
+				}
+				switch (pWorldModel[nCnt].nID)
+				{
+				case 0:
+					pWorldModel[nCnt].move.x = 5.f;
+					break;
+				case 1:
+					pWorldModel[nCnt].move.x = 10.f;
+					break;
+				case 2:
+					pWorldModel[nCnt].move.x = 15.f;
+					break;
+				case 3:
+					pWorldModel[nCnt].move.x = 20.f;
+					break;
+				}
+			}
+			else
+			{
+				if (pWorldModel[nCnt].pos.x >= WORLDMODEL_POS_RANGE_X)
+				{
+					pWorldModel[nCnt].pos.x = -WORLDMODEL_POS_RANGE_X;
+					pWorldModel[nCnt].pos.z = (float)(rand() % WORLDMODEL_POS_RANGE_Z - WORLDMODEL_POS_RANGE_Z / 2);
+
+					//当たり判定用boxの設定
+					int nNumVtx;//頂点数
+					DWORD sizeFvF;//一つ頂点formatのsize
+					BYTE *pVtxBuff;//頂点bufferへのpointer
+	
+					nNumVtx = pWorldModel[nCnt].Xfile_Mesh->GetNumVertices();//頂点数を取る
+					sizeFvF = D3DXGetFVFVertexSize(pWorldModel[nCnt].Xfile_Mesh->GetFVF());//formatの大きさ
+					pWorldModel[nCnt].Xfile_Mesh->LockVertexBuffer(D3DLOCK_READONLY, (void**)&pVtxBuff);//頂点buffをlock
+					for (int nCntVtx = 0; nCntVtx < nNumVtx; nCntVtx++)
+					{
+						D3DXVECTOR3 vtx = *(D3DXVECTOR3*)pVtxBuff;//頂点情報を取る
+						pVtxBuff += sizeFvF;//次に行く
+
+											//当たり判定用boxの頂点を探す
+						if (nCntVtx == 0)//初めの頂点
+						{
+							pWorldModel[nCnt].MaxColBox = vtx;
+							pWorldModel[nCnt].MinColBox = vtx;
+						}
+						else
+						{
+							if (vtx.x > pWorldModel[nCnt].MaxColBox.x) pWorldModel[nCnt].MaxColBox.x = vtx.x;
+							else if (vtx.x < pWorldModel[nCnt].MinColBox.x) pWorldModel[nCnt].MinColBox.x = vtx.x;
+							if (vtx.y > pWorldModel[nCnt].MaxColBox.y) pWorldModel[nCnt].MaxColBox.y = vtx.y;
+							else if (vtx.x < pWorldModel[nCnt].MinColBox.x) pWorldModel[nCnt].MinColBox.y = vtx.y;
+							if (vtx.z > pWorldModel[nCnt].MaxColBox.z) pWorldModel[nCnt].MaxColBox.z = vtx.z;
+							else if (vtx.x < pWorldModel[nCnt].MinColBox.x) pWorldModel[nCnt].MinColBox.z = vtx.z;
+						}
+					}
+					pWorldModel[nCnt].MaxColBox = pWorldModel[nCnt].pos + pWorldModel[nCnt].MaxColBox;
+					pWorldModel[nCnt].MinColBox = pWorldModel[nCnt].pos + pWorldModel[nCnt].MinColBox;//minboxの値は負数
+					pWorldModel[nCnt].Xfile_Mesh->UnlockVertexBuffer();//unlock
+				}
+				switch (pWorldModel[nCnt].nID)
+				{
+				case 0:
+					pWorldModel[nCnt].move.x = 5.f;
+					break;
+				case 1:
+					pWorldModel[nCnt].move.x = 10.f;
+					break;
+				case 2:
+					pWorldModel[nCnt].move.x = 15.f;
+					break;
+				case 3:
+					pWorldModel[nCnt].move.x = 20.f;
+					break;
+				}
+			}
+			pWorldModel[nCnt].pos += pWorldModel[nCnt].move;
+			pWorldModel[nCnt].MaxColBox += pWorldModel[nCnt].move;
+			pWorldModel[nCnt].MinColBox += pWorldModel[nCnt].move;
+		}
+		else
+		{
+
+		}
+	}
+}
+
+void DrawWorldModel(void)
+{
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();
+	D3DXMATRIX mtxRot, mtxTrans, mtxworld;
+	D3DMATERIAL9 matDef;
+
+	for (int nCnt = 0; nCnt < WORLDMODEL_NUM; nCnt++)
+	{
+		D3DXMatrixIdentity(&mtxworld);//単位matrix
+		D3DXMatrixRotationYawPitchRoll(&mtxRot, pWorldModel[nCnt].rot.y, pWorldModel[nCnt].rot.x, pWorldModel[nCnt].rot.z);//回転角度
+		D3DXMatrixMultiply(&mtxworld, &mtxworld, &mtxRot);//合算
+		D3DXMatrixTranslation(&mtxTrans, pWorldModel[nCnt].pos.x, pWorldModel[nCnt].pos.y, pWorldModel[nCnt].pos.z); //世界matrix全体　移動したい距離というmatrixの合算
+		D3DXMatrixMultiply(&mtxworld, &mtxworld, &mtxTrans); //合算　世界matrixの移動
+		pDevice->SetTransform(D3DTS_WORLD, &mtxworld); //set the world into Map
+		pDevice->GetMaterial(&matDef);//材質を取る
+		for (DWORD nCntMat = 0; nCntMat < (int)pWorldModel[nCnt].Xfile_MaterialNum; nCntMat++)
+		{
+			pDevice->SetMaterial(&pMaterials[nCntMat].MatD3D);
+			pDevice->SetTexture(0, pWorldModel[nCnt].g_Textures[nCntMat]);
+			pWorldModel[nCnt].Xfile_Mesh->DrawSubset(nCntMat);
+		}
+		pDevice->SetMaterial(&matDef);
+	}
+}
+
+MODEL *GetWorldModel(void)
+{
+	return pWorldModel;
+}
